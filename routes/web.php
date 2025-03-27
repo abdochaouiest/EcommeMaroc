fbf190484443cfdbb50e085a9a5e4bc3edcd1c4b
abdochaouiest <abderahmane.chaoui.estk@gmail.com>
2025-03-27 12:53:00 +0000

    Ajout des routes pour l'authentification et les dashboards (user & admin) avec middleware

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\StatiqueController;
+use App\Http\Middleware\IsAdmin;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [StatiqueController::class, 'index'])->name('home');
+
+
+Route::controller(AuthController::class)->group( function (){
+    Route::get('register','register')->name('register');
+    Route::post('register','registerSave')->name('register.save');
+    Route::get('login','login')->name('login');
+    Route::post('login','loginAction')->name('login.action');
+    Route::get('logout','logout')->middleware('auth:sanctum')->name('logout');
 });
+
+Route::middleware(['auth:sanctum'])->group(function () {// kay3ni blli ay wahed khaso ykoon connecté (authenticité) bach y9der ymchi l dashboard dyal user
+    Route::get('dashboard/{user}', [StatiqueController::class, 'dashboardUser'])->name('dashboard.user')->where([
+        'user' => "[0-9]+"
+    ]);
+});
+Route::middleware(['auth:sanctum','Isadmin'])->group(function () {//kay3ni blli khass tkoun connecté o admin bach t9der t'akhod l'accès l dashboard/admin.
+    Route::get('dashboard/admin', [StatiqueController::class, 'dashboardAdmin'])->name('dashboard.admin');
+});
+
+
