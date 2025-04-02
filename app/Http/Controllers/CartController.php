0a14b17cdba0cbe021f8797c2c13024554d9f187
abdochaouiest <abderahmane.chaoui.estk@gmail.com>
2025-04-02 15:58:03 +0000

    creation de controller de panier avec model

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cart;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CartController extends Controller
+{
+    public function index(){
+        $cartItems = Cart::where('user_id', Auth::id())->with('product')->get();
+        return view('cart.index', compact('cartItems'));
+
+    }
+    public function addToCart(Request $request,$productId)
+    {
+        $cartItem = Cart::where('user_id', Auth::id())->where('product_id', $productId)->first();
+
+        if ($cartItem) {
+            $cartItem->increment('quantity',$request->quantity);
+        } else {
+            Cart::create([
+                'user_id' => Auth::id(),
+                'product_id' => $productId,
+                'quantity' => $request->quantity
+            ]);
+        }
+
+        return back()->with('success', 'Produit ajouté au panier');
+    }
+    public function removeFromCart($productId)
+    {
+        Cart::where('user_id', Auth::id())->where('product_id', $productId)->delete();
+        return back()->with('success', 'Produit retiré du panier');
+    }
+}
