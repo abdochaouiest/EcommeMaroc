740ccccaa59b451d70cecd7a45c017495a0dae0d
abdochaouiest <abderahmane.chaoui.estk@gmail.com>
2025-04-03 14:08:23 +0000

    ajouter des modifications sur statique et cart controller avec cart model et user view

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,11 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Auth;
 
 class Cart extends Model
 {
     use HasFactory;
+
+    protected $table = 'cart';
+
+    protected $fillable = ['user_id', 'product_id', 'quantity'];
+
     public function product()
     {
         return $this->belongsTo(Product::class);
