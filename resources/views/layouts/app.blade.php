27038572a2942161ac5f7b321abebc8b4adb84a4
Mohamed <mohamed.abhry@edu.uiz.ac.ma>
2025-04-07 00:31:43 +0100

    feat: add About Us and Contact Us pages with shared layout
    - Created aboutus.blade.php and contactus.blade.php under index views
    - Updated app.blade.php layout with shared header and footer includes
    - Modularized footer and header into separate blade components

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,18 +1,35 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>@yield('title', 'My Laravel App')</title>
-</head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="author" content="Untree.co">
+  <link rel="shortcut icon" href="favicon.png">
+
+  <meta name="description" content="" />
+  <meta name="keywords" content="bootstrap, bootstrap4" />
+
+		<!-- Bootstrap CSS -->
+		<link href="{{ asset('index/css/bootstrap.min.css') }}" rel="stylesheet">
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
+		<link href="{{ asset('index/css/tiny-slider.css') }}" rel="stylesheet">
+		<link href="{{ asset('index/css/style.css') }}" rel="stylesheet">
+		<title>Furni Free Bootstrap 5 Template for Furniture and Interior Design Websites by Untree.co </title>
+	</head>
 <body>
-    <main>
-        @yield('contents')
-    </main>
 
-    <footer>
-        <p>&copy; 2025 My Website</p>
-    </footer>
+    <!-- {{-- Navbar --}} -->
+    @include('layouts.header')
 
+    <!-- {{-- Page Content --}} -->
+    @yield('contents')
+<!-- 
+    {{-- Footer --}} -->
+    @include('layouts.footer')
+
+    
+    <script src="{{ asset('index/js/bootstrap.bundle.min.js') }}"></script>
+	<script src="{{ asset('index/js/tiny-slider.js') }}"></script>
+	<script src="{{ asset('index/js/custom.js') }}"></script>	
 </body>
 </html>
